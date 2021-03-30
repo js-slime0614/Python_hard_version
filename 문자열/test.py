24b1e890e9d285471f1f254581484112d63d789d
단단한슬라임 <66393239+1409-pjs@users.noreply.github.com>
2021-03-30 19:53:56 +0900

    test2

--- 문자열/test.py
+++ 문자열/test.py
@@ -3,4 +3,14 @@ math = 55
 english = 75
 
 평균 = (korean + math + english) / 3
-print($"누군가의 평균 : {평균}")
+print(f"누군가의 평균 : {평균}")
+
+#--------------------------
+
+#13이 홀수인지 짝수인지
+num = 13
+num %= 2
+if num == 0 :
+    print("짝수")
+else : 
+    print("홀수")
